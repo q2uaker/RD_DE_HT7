7634168e20444b7f41ae7e9b9b3174afacb3bf24
q2uaker <65255047+q2uaker@users.noreply.github.com>
2021-10-24 18:21:57 +0300

    Update my_postgres_operators.py

--- HT7/my_postgres_operators.py
+++ HT7/my_postgres_operators.py
@@ -1,21 +1,3 @@
-# -*- coding: utf-8 -*-
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
@@ -86,7 +68,7 @@ class myPostgresOperator_TablesToCSV(BaseOperator):
 
     def execute(self, context):
         
-        file = open(self.tables_csv_filename,newline='')
+         file = open(self.tables_csv_filename,newline='')
         tables =[]
         with file:
             reader = csv.reader(file)
@@ -96,22 +78,15 @@ class myPostgresOperator_TablesToCSV(BaseOperator):
         self.hook = PostgresHook(postgres_conn_id=self.postgres_conn_id,
                                  schema=self.database)
         for table in tables:
-            sql_cols="SELECT column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '"+table[0]+"'"
-            cols = self.hook.get_records(sql_cols, parameters=self.parameters)
-            columns=[]
-            for col in cols:
-                columns.append(col[0])
-            
-            sql = "SELECT * FROM "+table[0]
+                        
+            sql = "COPY "+table[0]+ " TO STDOUT WITH HEADER CSV"
             self.log.info('SQL: %s', sql)
-            records = self.hook.get_records(sql, parameters=self.parameters)
+          
         
-            #Write to csv file
             filename = "_"+table[0]+".csv"
-            with open(self.folder+'/'+filename, 'w') as fp:
-                a = csv.writer(fp, quoting = csv.QUOTE_MINIMAL, delimiter = '|')
-                a.writerow(columns)
-                a.writerows(records)
+           
+            self.hook.copy_expert(sql, self.folder+'/'+filename)
+        
    
         for output in self.hook.conn.notices:
             self.log.info(output)
